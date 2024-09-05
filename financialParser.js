76393498a0453fd97298177ea0602d6345a96f7b
hhuziuk <huziukwork@gmail.com>
2024-09-05 23:08:46 +0200

    expanded financialParser.js

---
+++ financialParser.js
@@ -0,0 +1,120 @@
+import { Parser } from './parser.js';
+
+export class FinancialParser extends Parser {
+  constructor(url, maxRequestsPerHour = 4) {
+    super(url, maxRequestsPerHour);
+  }
+
+  extractValue(regex) {
+    const match = this.html.match(regex);
+    return match ? match[1].trim() : 'N/A';
+  }
+
+  get stockName() {
+    return this.extractValue(/<b>(.*?)<\/b>/);
+  }
+
+  get price() {
+    return this.extractValue(/Price.*?<b>(\d+\.\d+)<\/b>/);
+  }
+
+  get marketCapitalization() {
+    return this.extractValue(/Market Cap.*?<b>(.*?)<\/b>/);
+  }
+
+  get income() {
+    return this.extractValue(/Income.*?<b>(.*?)<\/b>/);
+  }
+
+  get sales() {
+    return this.extractValue(/Sales.*?<b>(.*?)<\/b>/);
+  }
+
+  get bookSh() {
+    return this.extractValue(/Book\/sh<\/td><td[^>]*><b>(.*?)<\/b>/);
+  }
+
+  get cashSh() {
+    return this.extractValue(/Cash\/sh<\/td><td[^>]*><b>(.*?)<\/b>/);
+  }
+
+  get dividendEstimation() {
+    return this.extractValue(/Dividend Estimate.*?<b>(.*?)<\/b>/);
+  }
+
+  get dividendTtm() {
+    return this.extractValue(/Dividend TTM.*?<b>(.*?)<\/b>/);
+  }
+
+  get peRatio() {
+    return this.extractValue(/P\/E<\/td><td[^>]*><b>(.*?)<\/b>/);
+  }
+
+  get forwardPeRatio() {
+    return this.extractValue(/Forward P\/E<\/td><td[^>]*><b>(.*?)<\/b>/);
+  }
+
+  get pegRatio() {
+    return this.extractValue(/PEG<\/td><td[^>]*><b><span[^>]*>(\d+(\.\d+)?)<\/span><\/b>/);
+  }
+
+  get psRatio() {
+    return this.extractValue(/P\/S<\/td><td[^>]*><b>(.*?)<\/b>/);
+  }
+
+  get pbRatio() {
+    return this.extractValue(/P\/B<\/td><td[^>]*><b><span[^>]*>(\d+(\.\d+)?)<\/span><\/b>/);
+  }
+
+  get pcRatio() {
+    return this.extractValue(/P\/С<\/td><td[^>]*><b><span[^>]*>(\d+(\.\d+)?)<\/span><\/b>/);
+  }
+
+  get pFcfRatio() {
+    return this.extractValue(/P\/FCF<\/td><td[^>]*><b>(.*?)<\/b>/);
+  }
+
+  get quickRatio() {
+    return this.extractValue(/Quick Ratio.*?<b>(.*?)<\/b>/);
+  }
+
+  get currentRatio() {
+    return this.extractValue(/Current Ratio.*?<span class="color-text .*?">(.*?)<\/span>/);
+  }
+
+  get debtEq() {
+    return this.extractValue(/Debt\/Eq<\/td><td[^>]*><b><span[^>]*>(\d+(\.\d+)?)<\/span><\/b>/);
+  }
+
+  get ltDebtEq() {
+    return this.extractValue(/LT Debt\/Eq<\/td><td[^>]*><b><span[^>]*>(\d+(\.\d+)?)<\/span><\/b>/);
+  }
+
+  get epsTtm() {
+    return this.extractValue(/EPS (ttm)<\/td><td[^>]*><b>(.*?)<\/b>/);
+  }
+
+  get roa() {
+    return this.extractValue(/ROA.*?<span class="color-text .*?">(.*?)<\/span>/);
+  }
+
+  get roe() {
+    return this.extractValue(/ROE.*?<span class="color-text .*?">(.*?)<\/span>/);
+  }
+
+  get roi() {
+    return this.extractValue(/ROI.*?<span class="color-text .*?">(.*?)<\/span>/);
+  }
+
+  get volume() {
+    return this.extractValue(/Volume.*?<b>(.*?)<\/b>/);
+  }
+
+  get beta() {
+    return this.extractValue(/Beta.*?<b>(.*?)<\/b>/);
+  }
+
+  get change() {
+    return this.extractValue(/Change.*?<span class="color-text .*?">(.*?)<\/span>/);
+  }
+}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { Parser } from './parser.js';
+import { FinancialParser } from './financialParser.js';
 
 const url = 'https://finviz.com/quote.ashx?t=AAPL&p=d';
-const parser = new Parser(url);
+const parser = new FinancialParser(url);
 
-let requestCount = 0; // Request counter
+let requestCount = 0;
 
 const fakeReq = {
   headers: {
@@ -16,7 +16,6 @@ const fakeReq = {
 
 const ip = parser.getClientIp(fakeReq);
 
-
 const intervalId = setInterval(async () => {
   requestCount++;
   console.log(`Request #${requestCount}`);
@@ -27,8 +26,36 @@ const intervalId = setInterval(async () => {
   if (canRequest) {
     try {
       await parser.fetchHTML();
-      parser.parseData();
-      parser.printData();
+
+      // Extract and print data from the parsed HTML using the financialParser methods
+      console.log('Stock Name:', parser.stockName);
+      console.log('Price:', parser.price);
+      console.log('Market Cap:', parser.marketCapitalization);
+      console.log('P/E Ratio:', parser.peRatio);
+      console.log('Income:', parser.income);
+      console.log('Sales:', parser.sales);
+      console.log('Book/Share:', parser.bookSh);
+      console.log('Cash/Share:', parser.cashSh);
+      console.log('Dividend Estimate:', parser.dividendEstimation);
+      console.log('Dividend TTM:', parser.dividendTtm);
+      console.log('Forward P/E Ratio:', parser.forwardPeRatio);
+      console.log('PEG Ratio:', parser.pegRatio);
+      console.log('PS Ratio:', parser.psRatio);
+      console.log('PB Ratio:', parser.pbRatio);
+      console.log('PC Ratio:', parser.pcRatio);
+      console.log('P/FCF Ratio:', parser.pFcfRatio);
+      console.log('Quick Ratio:', parser.quickRatio);
+      console.log('Current Ratio:', parser.currentRatio);
+      console.log('Debt/Equity:', parser.debtEq);
+      console.log('Long Term Debt/Equity:', parser.ltDebtEq);
+      console.log('EPS TTM:', parser.epsTtm);
+      console.log('ROA:', parser.roa);
+      console.log('ROE:', parser.roe);
+      console.log('ROI:', parser.roi);
+      console.log('Volume:', parser.volume);
+      console.log('Beta:', parser.beta);
+      console.log('Change:', parser.change);
+
     } catch (err) {
       console.error('Request error:', err.message);
     }
